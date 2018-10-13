7fe5020d6841d1e27dbdc0d05eacbbd1373881ff
Fabiola Haskell <fabiolahaskell@fabiolas-air.frontierlocal.net>
2018-10-13 14:14:04 -0500

    Initial Commit

---
+++ Side Menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Side Menu
+//
+//  Created by Fabiola Haskell on 10/13/18.
+//  Copyright © 2018 Eric Haskell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
